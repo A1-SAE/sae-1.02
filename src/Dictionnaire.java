a28f2cfc9255699ce385ae3a1cce5dae79912c59
Rafiki13 <58651523+Rafiki13@users.noreply.github.com>
2022-01-15 19:03:15 +0100

    questions 22-24

--- src/Dictionnaire.java
+++ src/Dictionnaire.java
@@ -1,10 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Dictionnaire {
     HTNaive htn;
 
     public Dictionnaire(int m){
         this.htn = new HTNaive(m);
+    }
+
+    public Dictionnaire(String fileName, int m){
+        this.htn = new HTNaive(Dictionnaire.calculeListeInt(fileName), m);
+    }
+
+    public Dictionnaire(String fileName, double f){
+        this.htn = new HTNaive(Dictionnaire.calculeListeInt(fileName), f);
     }
 
     public static BigInteger stringToBigInteger(String s){
@@ -45,4 +56,35 @@ public class Dictionnaire {
     public String toStringV2(){
         return this.htn.toStringV2();
     }
+
+    public static ListeBigI calculeListeInt(String fileName){
+        File f = new File(fileName);
+        ListeBigI res = new ListeBigI();
+        Scanner sc;
+        //un scanner est un objet permettant de "scanner" (parcourir)
+        //une entrée (clavier, ou une chaîne, ou un File, etc)
+        try {
+            sc = new Scanner(f);
+            //ici on construit le scanner avec comme entrée f
+            //cette construction peut échouer (si par exemple fichier.txt n’existe pas)
+        }
+        catch(FileNotFoundException e){
+            //si la construction échoue, on passe ici
+            System.out.println(("problème d’accès au fichier " + e.getMessage()));
+            return new ListeBigI();
+        }
+        sc.useDelimiter(" |\\n|,|;|:|\\.|!|\\?|-");
+        //on définit les délimiteurs comme le caractère ’\n’, le caractère ’,’ etc...
+        //cela définit maintenant la notion de "morceau" comme une suite
+        //de caractères entre deux délimiteurs
+        ListeBigI mots = new ListeBigI();
+        while (sc.hasNext()) { //sc.hasNext() renvoie vrai ssi
+            //il reste encore un morceau à découvrir dans f
+            String mot = sc.next(); //sc.next() renvoie le prochain morceau
+            if(mot.length() > 0) mots.ajoutTete(Dictionnaire.stringToBigInteger(mot));
+        }
+
+        return mots;
+    }
+
 }
--- src/TestsMain.java
+++ src/TestsMain.java
@@ -3,11 +3,11 @@ import java.math.BigInteger;
 public class TestsMain {
     public static void main(String[] args){
         /* Q3 */
-        HTNaive htn1 = new HTNaive(5);
-        HTNaive htn2 = new HTNaive(10);
+        /*HTNaive htn1 = new HTNaive(5);
+        HTNaive htn2 = new HTNaive(10);*/
 
         /* Q6/8 */
-        htn1.ajout(BigInteger.valueOf(500));
+        /*htn1.ajout(BigInteger.valueOf(500));
         htn1.ajout(BigInteger.valueOf(502));
         htn1.ajout(BigInteger.valueOf(502));
         System.out.println(htn1);
@@ -26,37 +26,60 @@ public class TestsMain {
         htn1.ajoutListe(l1);
         htn1.ajoutListe(l2);
         System.out.println(htn1);
-        System.out.println(htn2);
+        System.out.println(htn2);*/
 
         /* Q7 */
-        System.out.println(htn1.contient(BigInteger.valueOf(50)));
+        /*System.out.println(htn1.contient(BigInteger.valueOf(50)));
         System.out.println(htn1.contient(BigInteger.valueOf(500)));
         System.out.println(htn1.contient(BigInteger.valueOf(503)));
-        System.out.println(htn1.contient(BigInteger.valueOf(-500)));
+        System.out.println(htn1.contient(BigInteger.valueOf(-500)));*/
 
         /* Q9 */
-        System.out.println(htn1.getElements().toString());
-        System.out.println(htn2.getElements().toString());
+        /*System.out.println(htn1.getElements().toString());
+        System.out.println(htn2.getElements().toString());*/
 
         /* Q12 */
-        System.out.println(htn1.getNbListes());
-        System.out.println(htn2.getNbListes());
+        /*System.out.println(htn1.getNbListes());
+        System.out.println(htn2.getNbListes());*/
 
         /* Q13 */
-        System.out.println(htn1.toStringV2());
-        System.out.println(htn2.toStringV2());
+        /*System.out.println(htn1.toStringV2());
+        System.out.println(htn2.toStringV2());*/
 
         /* Q14 */
-        HTNaive htn3 = new HTNaive(l1, 5);
+        /*HTNaive htn3 = new HTNaive(l1, 5);
         System.out.println(htn3);
 
         HTNaive htn4 = new HTNaive(l2, 5);
-        System.out.println(htn4);
+        System.out.println(htn4);*/
 
         /* Q15 */
-        HTNaive htn5 = new HTNaive(l1, (double) 2);
-        System.out.println(htn5);
+        /*HTNaive htn5 = new HTNaive(l1, (double) 2);
+        System.out.println(htn5);*/
 
-        System.out.println(Dictionnaire.stringToBigInteger("Manger"));
+        /* Q16 */
+        /*Dictionnaire d1 = new Dictionnaire(5);*/
+
+        /* Q19 */
+        /*d1.ajout("Manger");
+        d1.ajout("Bite");*/
+
+        /* Q20 */
+        /*System.out.println(d1.contient("Manger"));
+        System.out.println(d1.contient("rien"));
+        System.out.println(d1.contient("manger"));*/
+
+        /* Q21 */
+        /*System.out.println(d1.getCardinal());
+        System.out.println(d1.getMaxSize());
+        System.out.println(d1.getNbListes());
+        System.out.println(d1);
+        System.out.println(d1.toStringV2());*/
+
+        /* Q23 */
+        /*ListeBigI mots = Dictionnaire.calculeListeInt("randomWordsPetit.txt");
+        System.out.println(mots);*/
+
+
     }
 }
